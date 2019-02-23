12832dd174c0e80c7f87472f435c3bbcca67a8ce
MasterLopez <javy7@hotmail.com>
2019-02-22 20:01:20 -0400

    Changes

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ class App extends Component {
 
     return (
       <div className="container">
+        <h1>Form using React JS</h1>
         <Table
           characterData={characters}
           removeCharacter={this.removeCharacter}
